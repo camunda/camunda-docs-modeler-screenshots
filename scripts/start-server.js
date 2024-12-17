c860974f49d5cd5aca30fe4c73e5482e636ff821
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2024-12-17 16:55:31 +0100

    chore: migrate to eslint@9

--- scripts/start-server.js
+++ scripts/start-server.js
@@ -1,6 +1,6 @@
 const { spawn } = require('node:child_process');
 
-const subprocess = spawn('node', ['./scripts/server.js'], {
+const subprocess = spawn('node', [ './scripts/server.js' ], {
   detached: true,
   stdio: 'ignore'
 });
